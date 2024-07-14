61082ddff7b956d00fb45951737d4f729672cc5e
Madhusudan Babar <madhusudan.babar@DM2020.datametica.com>
2024-07-14 15:16:35 +0530

    cron job setup

--- app/api/crawl/route.js
+++ app/api/crawl/route.js
@@ -1,44 +1,53 @@
 import { NextResponse } from "next/server";
 import { API_ENDPOINT } from "./constants";
 import { MongoClient } from "mongodb";
+import cron from "node-cron";
 
-export async function GET() {
+const getStocks = async () => {
   const client = new MongoClient(process.env.MOGO_URI);
 
-  console.log(process.env.MOGO_URI);
+  await client.connect();
+
+  const db = client.db("fomo");
+  const collection = db.collection("crypto");
+
+  const response = await fetch(API_ENDPOINT, {
+    method: "POST",
+    headers: {
+      "x-api-key": process.env.COINS_API_KEY,
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      currency: "USD",
+      sort: "rank",
+      order: "ascending",
+      offset: 0,
+      limit: 5,
+      meta: false,
+    }),
+  });
+
+  if (!response.ok) {
+    throw new Error("Failed to fetch data");
+  }
+
+  const data = await response.json();
+  collection.insertMany(data);
+
+  return data;
+};
+
+cron.schedule("0 * * * *", () => {
+  console.log("Running hourly task");
+  getStocks();
+});
+
+export async function GET() {
   try {
-    await client.connect();
+    const stocks = await getStocks();
 
-    const db = client.db("fomo");
-    const collection = db.collection("crypto");
-
-    const response = await fetch(API_ENDPOINT, {
-      method: "POST",
-      headers: {
-        "x-api-key": process.env.COINS_API_KEY,
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({
-        currency: "USD",
-        sort: "rank",
-        order: "ascending",
-        offset: 0,
-        limit: 5,
-        meta: false,
-      }),
-    });
-
-    if (!response.ok) {
-      throw new Error("Failed to fetch data");
-    }
-
-    const data = await response.json();
-
-    collection.insertMany(data);
-
-    return NextResponse.json({ data });
+    return NextResponse.json({ stocks });
   } catch (error) {
-    // console.error("Error fetching data:", error);
     return NextResponse.error();
   }
 }
